775ed1c71d718ae4a8f38bc45a37126a654d4f06
starkgith465 <matheus.santos.gith@gmail.com>
2025-02-20 17:02:49 +0000

    SUPER TRUNFO DESAFIO CONCLUIDO FOI O QUE EU CONSSEGUI FAZER

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,4 +1,37 @@
 #include <stdio.h>
 
-int main(){;
+int main() {
+    char estado1, estado2;
+    char codigo1[4], codigo2[4];
+    char nomeCidade1[50], nomeCidade2[50];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosTuristicos1, pontosTuristicos2;
 
+    printf("Insira os dados da Carta 1:\n");
+    scanf(" %c %s %s %d %f %f %d", &estado1, codigo1, nomeCidade1, &populacao1, &area1, &pib1, &pontosTuristicos1);
+
+    printf("\nInsira os dados da Carta 2:\n");
+    scanf(" %c %s %s %d %f %f %d", &estado2, codigo2, nomeCidade2, &populacao2, &area2, &pib2, &pontosTuristicos2);
+
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+
+    return 0;
+}
