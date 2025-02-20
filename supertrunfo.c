19f0e4d8e51794e7c181b89adb6e0891e78ffdc0
starkgith465 <matheus.santos.gith@gmail.com>
2025-02-20 18:07:08 +0000

    pib

--- supertrunfo.c
+++ supertrunfo.c
@@ -21,7 +21,7 @@ int main() {
     printf("Nome da Cidade: %s\n", nomeCidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("pib: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
     printf("\nCarta 2:\n");
@@ -30,7 +30,7 @@ int main() {
     printf("Nome da Cidade: %s\n", nomeCidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("pib: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
 
     return 0;
